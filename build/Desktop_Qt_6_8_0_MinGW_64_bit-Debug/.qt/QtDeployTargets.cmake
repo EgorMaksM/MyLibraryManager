d46b51d8acff679be44bcb9031a057378ed0109f
yehorkk <keynikus@gmail.com>
2025-01-14 14:48:11 +0100

    Migrate to qmake from CMAKE

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MyLibraryManagerQT_FILE C:/Users/yehmak001/OneDrive - IKT Agder/YFF/MyLibraryManagerQT/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/MyLibraryManagerQT.exe)
-set(__QT_DEPLOY_TARGET_MyLibraryManagerQT_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MyLibraryManagerQT_RUNTIME_DLLS C:/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
